0f6591594e1ceed81b23ecfaff9e4732a0e0b526
LM <236772625@qq.com>
2023-06-29 00:41:23 +0800

    springboot基础框架

---
+++ src/main/java/com/lzh/genericframework/GenericFrameworkApplication.java
@@ -0,0 +1,13 @@
+package com.lzh.genericframework;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GenericFrameworkApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GenericFrameworkApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/lzh/genericframework/GenericFrameworkApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lzh.genericframework;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GenericFrameworkApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
